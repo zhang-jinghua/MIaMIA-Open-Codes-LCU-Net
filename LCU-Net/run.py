5b5a0df60a9b144cd5735084ceb86ae10ab67c1b
JackTwist0512 <41134183+JackTwist0512@users.noreply.github.com>
2021-02-24 17:59:47 +0800

    Add files via upload

--- LCU-Net/run.py
+++ LCU-Net/run.py
@@ -1,1 +1,10 @@
-1
+from main import*
+from evaluation import*
+
+# main(26,3)
+main(26,13)
+# for i in range(0,21):
+#       n = [0, 1, 2]
+#       evaluation(i, n[1])
+#       evaluation(i, n[2])
+#       evaluation(i, n[0])
